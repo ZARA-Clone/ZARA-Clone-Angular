79fefa2f939cb9f77aabd8ad0f52447a3c163a51
Menna Deyaa <mennadeyaam@gmail.com>
2024-03-25 21:14:20 +0200

    productbrowse0

---
+++ src/Components/product-browse/product-browse.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-browse',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-browse.component.html',
+  styleUrl: './product-browse.component.css'
+})
+export class ProductBrowseComponent {
+
+}
