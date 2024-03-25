988e40af8ffa86df45d8cf983bad5269132c47fe
Menna Deyaa <mennadeyaam@gmail.com>
2024-03-25 21:09:21 +0200

    footerfix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FooterComponent } from '../Component/footer/footer.component';
+import { FooterComponent } from '../Components/footer/footer.component';
 
 
 @Component({
