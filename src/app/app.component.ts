51d2f9c28dc2b4d070a5b9d13365e59668ebb24d
Menna Deyaa <mennadeyaam@gmail.com>
2024-03-25 20:49:18 +0200

    footercomponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ProductDetailsComponent } from '../Components/product-details/product-details.component';
-import { HeaderComponent } from '../Components/header/header.component';
+import { FooterComponent } from '../Component/footer/footer.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProductDetailsComponent,HeaderComponent],
+  imports: [RouterOutlet,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
