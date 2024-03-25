68805084274715c6f64a28ae8d2ba8bfd95f3092
Elsayed Reda <elsayedreda760@gmail.com>
2024-03-25 17:21:03 +0200

    Handle upload images success

--- src/app/Components/Dashboard/Products/add-product/add-product.component.ts
+++ src/app/Components/Dashboard/Products/add-product/add-product.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductOperationsService } from '../../../../Services/Dashboard/product-operations.service';
-import { FormBuilder, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
 import { AddProductDto } from '../../../../Dtos/Dashboard/add-product-dto';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
-import { ToastrModule, ToastrService } from 'ngx-toastr';
+import { ToastrModule } from 'ngx-toastr';
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatOption } from '@angular/material/core';
 import { NgIf } from '@angular/common';
@@ -15,77 +15,44 @@ import { NgIf } from '@angular/common';
   templateUrl: './add-product.component.html',
   styleUrl: './add-product.component.css'
 })
-export class AddProductComponent implements OnInit {
+export class AddProductComponent {
   product: AddProductDto = new AddProductDto();
-  productForm!: FormGroup
   ImageUrls: string[] = [];
   selectedBrand: number = 0;
-  selectedImage: File | null = null;
-  constructor(private _http: HttpClient
-    , private _formBuilder: FormBuilder, private _productsService: ProductOperationsService
-    , private router: Router) { }
+  selectedImages: File[] = [];
+  constructor(private _productsService: ProductOperationsService, private router: Router) { }
 
-  ngOnInit(): void {
-    // this.productForm = this._formBuilder.group({
-    //   name: ["", [Validators.required, Validators.minLength(3)]],
-    //   price: [null, [Validators.required, Validators.min(50), Validators.max(2000)]],
-    //   quantity: [1, [Validators.required]],
-    //   discount: 0,
-    //   categoryID: [0, [Validators.required]],
-    //   description: ["", Validators.maxLength(500)],
-    //   ImageUrls: []
-  }
-  // onSubmit(productForm: NgForm) {
-  //   // console.log(this.productForm.value)
-  //   // this._productsService.add(this.productForm.value).subscribe(({
-  //   //   next: () => {
-  //   //     alert('Product has been added successfully')
-  //   //   },
-  //   //   error: (err) => {
-  //   //     alert(err);
-  //   //   }
-  //   // }));
-  //   if (productForm.invalid)
-  //     return;
-  //   this._productsService
-  //     .upload2(this.selectedImages)
-  //     .subscribe({
-  //       next: (responses: any) => {
-  //         console.log(responses);
-  //         this.ImageUrls = responses;
-  //         this.product.Name = this.product.Name.trim(),
-  //           this.product.Price = +this.product.Price;
-  //         this.product.Description = this.product.Description;
-  //         this.product.ImageUrls = this.ImageUrls;
-  //         this.product.Discount = this.product.Discount;
-  //         this.product.BrandID = this.selectedBrand;
-  //         this._productsService.add(this.product).subscribe(
-  //           {
-  //             next: (data) => { console.log(data); this.router.navigate(["dashboard/products"]) },
-  //             error: (err) => { console.log(err) },
-  //             complete: () => {
-  //               console.log("product adding success")
-  //             }
-  //           })
-  //         this.toastr.success("product adding success");
-  //       },
-  //       error: (err: any) => { console.log("error uploading photos:", err); }
-  //     })
-  // }
-  // uploadImage(event: any) {
-  //   this.productForm.value.Imageurls = event.target.files;
-  //   console.log(this.productForm.value.ImageUrls)
-  // }
-
-  // uploadPhotos(e: Event) {
-  //   const input = e.target as HTMLInputElement;
-  //   this.selectedImages = Array.prototype.slice.call(input.files) as File[];
-  //   console.log(this.selectedImages);
-  // }
   onFileSelected(event: any) {
-    this.selectedImage = event.target.files[0];
+    const input = event.target as HTMLInputElement;
+    this.selectedImages = Array.prototype.slice.call(input.files) as File[];
   }
-  onSubmit() {
-    this._productsService.upload3(this.selectedImage);
+  onSubmit(productForm: NgForm) {
+    if (productForm.invalid)
+      return;
+    this._productsService.upload(this.selectedImages).subscribe({
+      next: (responses: any) => {
+        console.log("res: " + responses);
+        this.ImageUrls = responses;
+        this.product.Name = this.product.Name.trim();
+        this.product.Price = this.product.Price;
+        this.product.Description = this.product.Description;
+        this.product.ImageUrls = this.ImageUrls;
+        this.product.Discount = this.product.Discount;
+        this.product.Quantity = this.product.Quantity;
+        this.product.BrandID = this.selectedBrand;
+        this._productsService.add(this.product).subscribe(
+          {
+            next: (data) => { console.log("data: " + data); this.router.navigate(["dashboard"]) },
+            error: (err) => { console.log("err: " + err) },
+            complete: () => {
+              console.log("Product has been added successfully")
+            }
+          })
+        alert("Product has been added successfully");
+      },
+      error: (err: any) => {
+        console.log("error uploading photos:", err);
+      }
+    })
   }
 }
--- src/app/Services/Dashboard/product-operations.service.ts
+++ src/app/Services/Dashboard/product-operations.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@angular/core';
 import { AddProductDto } from '../../Dtos/Dashboard/add-product-dto';
 import { environment } from '../../../environments/environment';
 import { Observable, catchError, throwError } from 'rxjs';
-import { IFileDto } from '../../Dtos/Dashboard/IFileDto.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -15,13 +14,11 @@ export class ProductOperationsService {
   });
 
   add(product: AddProductDto): Observable<AddProductDto> {
-    let result = this._httpClient.post<AddProductDto>(
-      `${environment.BASEURL}/api/Dashboard`, product
-      , { headers: this.httpHeaders });
-    return result
-      .pipe(
-        catchError(this.handleError)
-      );
+    let result = this._httpClient.post<AddProductDto>(`${environment.BASEURL}/api/Dashboard`
+      , product, { headers: this.httpHeaders });
+    return result.pipe(
+      catchError(this.handleError)
+    );
   }
 
   edit(id: number, product: AddProductDto) {
@@ -38,40 +35,14 @@ export class ProductOperationsService {
     else
       console.error(
         `Backend returned code ${error.status}, body was: `, error.error);
-
     return throwError(() => new Error('Something bad happened; please try again later.'));
   }
 
-  upload(file: File): Observable<IFileDto> {
-    var form = new FormData();
-    form.append("file", file)
-    console.log(file);
-    return this._httpClient.post<IFileDto>(`${environment.BASEURL}/api/Dashboard/upload`, form)
-  }
-
-  upload2(files: File[]): any {
-    console.log(files)
-    const form = new FormData();
+  upload(files: File[]): any {
+    let form = new FormData();
     files.forEach(file => {
-      form.append("files", file);
+      form.append("files", file, file.name);
     })
     return this._httpClient.post(`${environment.BASEURL}/api/Dashboard/upload`, form);
   }
-  upload3(selectedImage: File | null) {
-    if (selectedImage) {
-      const form = new FormData();
-      form.append("file", selectedImage, selectedImage.name);
-      return this._httpClient.post(`${environment.BASEURL}/api/Dashboard/upload`, form).subscribe({
-        next: (response) => {
-          console.log('Image uploaded successfully:', response);
-        },
-        error: (error) => {
-          console.error('Error uploading image:', error);
-        }
-      })
-    }
-    else
-      return null;
-  }
-
 }
