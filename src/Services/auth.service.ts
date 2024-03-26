baa138694e2ea8d20631d0bf28b545f252587cbe
BashiraTarek <bashiratarekahmed@gmail.com>
2024-03-27 01:37:04 +0200

    add service

---
+++ src/Services/auth.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private _HttpClient:HttpClient) { }
+
+  baseUrl:string="url";//ha3ml property asasya shayla el link da badal ma akraro kol shwya bs na's klmt login ha3mlha concate 
+login(UserData:object):Observable<any>{
+  return this._HttpClient.get(this.baseUrl,UserData)
+}
+
+
+}
