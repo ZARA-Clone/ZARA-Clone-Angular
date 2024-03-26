7037a002ca339222e5c68f86c9e2d134d6e0f303
Menna Deyaa <mennadeyaam@gmail.com>
2024-03-27 00:50:34 +0200

    homeone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ProductDetailsComponent } from '../Components/product-details/product-details.component';
-import { HeaderComponent } from '../Components/header/header.component';
+import { HomeComponent } from '../Components/home/home.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProductDetailsComponent,HeaderComponent],
+  imports: [RouterOutlet,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
