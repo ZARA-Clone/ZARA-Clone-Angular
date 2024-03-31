de7d830d6efcc91dbe28167cc2e5bd483d95defa
BashiraTarek <bashiratarekahmed@gmail.com>
2024-03-31 09:47:23 +0200

    edit wishlist design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WishlistComponent } from '../Components/wishlist/wishlist.component';
 
 
 @Component({
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { WishlistComponent } from '../Components/wishlist/wishlist.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'wishlist',component:WishlistComponent}
+];
