b1b6ea292f7fc0fd161ecdf747d4a83fe59a503f
BashiraTarek <bashiratarekahmed@gmail.com>
2024-04-03 08:10:27 +0200

    add new page to aboutus

---
+++ src/Components/aboutuss/confirmemail/confirmemail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-confirmemail',
+  standalone: true,
+  imports: [],
+  templateUrl: './confirmemail.component.html',
+  styleUrl: './confirmemail.component.css'
+})
+export class ConfirmemailComponent {
+
+}
