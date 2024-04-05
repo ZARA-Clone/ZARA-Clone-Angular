0812eeed9ee40469c4a9b204f97b7c798d9250cc
Yasser <yasserhhh161@gmail.com>
2024-04-05 13:03:15 +0200

    Fixed Tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ProductDetailsComponent } from '../Components/product-details/product-details.component';
 import { HeaderComponent } from '../Components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProductDetailsComponent,HeaderComponent],
+  imports: [RouterOutlet,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
