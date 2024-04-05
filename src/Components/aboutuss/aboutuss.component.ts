4c5814399ee9591630a5bd927315827570048511
BashiraTarek <bashiratarekahmed@gmail.com>
2024-04-05 13:50:57 +0200

    adding package for dialog and add dialog

--- src/Components/aboutuss/aboutuss.component.ts
+++ src/Components/aboutuss/aboutuss.component.ts
@@ -2,15 +2,25 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { DialogModule } from 'primeng/dialog';
+
+// @Component({
+//   selector: 'app-aboutuss',
+//   standalone: true,
+//   imports: [FormsModule,CommonModule,DialogModule],
+//   templateUrl: './cart.component.html',
+//   styleUrl: './cart.component.css'
+// })
 
 @Component({
   selector: 'app-aboutuss',
   standalone: true,
-  imports: [FormsModule,CommonModule],
+  imports: [FormsModule,CommonModule,DialogModule],
   templateUrl: './aboutuss.component.html',
   styleUrl: './aboutuss.component.css'
 })
 export class AboutussComponent {
+
 
   email: string = '';
   isValidEmail: boolean = false;
@@ -22,6 +32,8 @@ export class AboutussComponent {
   isButtonClicked: boolean = false;
   inputData:string='';
 
+
+  visible: boolean = false;
   constructor(private router:Router){}
 
   checkEmailValidity() {
@@ -42,6 +54,46 @@ export class AboutussComponent {
     return ['Woman', 'Man', 'Kids' , 'Beauty'];
   }
 
+  acceptTerms: boolean = false;
+  isSelected: boolean = false;
+  showErrorDialog: boolean = false;
+  showTermsDialog: boolean = false;
+  SetSelected() {
+    this.isSelected = true;
+  }
+ToogleTerms() {
+    this.acceptTerms = !this.acceptTerms;
+    console.log(this.acceptTerms);
+  }
+  // checkInputs() {
+     
+  //      if (!this.acceptTerms) {
+  //       this.showTermsDialog = true;
+  //       return;
+  //     }
+  //     else if (!this.isSelected) {
+  //       this.showErrorDialog = true;
+  //       return;
+  //     }
+  //     else {
+  //       this.router.navigate(['/confirmfemail'], { queryParams: { data: this.inputData } });
+  //      }
+  //   }
+  checkInputs() {
+    if (!this.acceptTerms) {
+      this.showTermsDialog = true;
+    } else if (!this.isSelected) {
+      this.showErrorDialog = true;
+    } else {
+      this.router.navigate(['/confirmfemail'], { queryParams: { data: this.inputData } });
+    }
+  }
+  
+  
+    printInputData() {
+      this.router.navigate(['/confirmfemail'], { queryParams: { data: this.inputData } });
+    }
+
   selectItem(item: string): void {
     this.selectedItems[item] = !this.selectedItems[item];
   }
@@ -55,7 +107,11 @@ export class AboutussComponent {
     this.printClicked = true;
   }
 
-  printInputData() {
-    this.router.navigate(['/confirmfemail'], { queryParams: { data: this.inputData } });
+  
+  display: boolean = false;
+
+  showDialog() {
+    this.visible = true;
   }
+
 }
